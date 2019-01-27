db052983a132db58a7ac137635f7d04782a1b21b
Brian Kendall <brian@briankendall.net>
2019-01-27 10:58:49 -0500

    Mouth animation and sfx

--- Assets/MainController.cs
+++ Assets/MainController.cs
@@ -23,7 +23,9 @@ public class MainController : MonoBehaviour
         public float inputFire = 0;
         
         public string dialogue = "";
+        public VoiceSynth.VoiceStyle voiceStyle = VoiceSynth.VoiceStyle.Normal;
         public double lastLetterTimestamp = 0;
+        public double nextMouthMovementTimestamp = 0;
         
         public State Clone() {
             return (State)MemberwiseClone();
@@ -33,16 +35,23 @@ public class MainController : MonoBehaviour
     const double textCharacterEntryDuration = 0.04;
     const double textCharacterEntryDurationFast = 0.0;
     const int dialogueMaxLineLength = 28;
+    const float minMouthMovementDuration = 0.08f;
+    const float maxMouthMovementDuration = 0.15f;
     
     Text dialogueText;
     Text optionsText;
+    VoiceSynth voiceSynth;
+    SpriteRenderer mouthSpriteRenderer;
     State state = new State();
     State previousState = new State();
-    
+    Bucket mouthSpriteBucket = new Bucket(1);
     
     void Start() {
         dialogueText = dialogueBox.GetComponent<Text>();
         optionsText = optionsBox.GetComponent<Text>();
+        voiceSynth = GetComponent<VoiceSynth>();
+        mouthSpriteRenderer = mouthSprite.GetComponent<SpriteRenderer>();
+        mouthSpriteBucket.FillBucketWithRange(-1, 3);
         
         dialogueText.text = "";
         optionsText.text = "";
@@ -60,6 +69,7 @@ public class MainController : MonoBehaviour
         state.inputFire = Input.GetAxis("Fire1");
         
         if (dialogueText.text.Length < state.dialogue.Length) {
+            voiceSynth.talking = true;
             double nextLetterTimestamp = state.lastLetterTimestamp + ((state.inputFire > 0) ? textCharacterEntryDurationFast : textCharacterEntryDuration);
             
             if (time >= nextLetterTimestamp) {
@@ -77,9 +87,30 @@ public class MainController : MonoBehaviour
                 dialogueText.text += nextLetter;
                 state.lastLetterTimestamp = time;
             }
+            
+            if (time >= state.nextMouthMovementTimestamp) {
+                pickRandomMouthSprite();
+                state.nextMouthMovementTimestamp = time + Random.Range(minMouthMovementDuration, maxMouthMovementDuration);
+            }
+            
+        } else {
+            voiceSynth.talking = false;
+            mouthSpriteRenderer.enabled = false;
         }
         
         previousState = holdPreviousState;
+    }
+    
+    void pickRandomMouthSprite() {
+        int choice = mouthSpriteBucket.Take();
+        
+        if (choice == -1) {
+            mouthSpriteRenderer.enabled = false;
+            return;
+        }
+        
+        mouthSpriteRenderer.enabled = true;
+        mouthSpriteRenderer.sprite = mouthSprites[choice];
     }
     
     string findNextWordInTextAfterIndex(string text, int index) {
