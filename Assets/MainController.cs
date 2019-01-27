50c4d0bba572c5ab8a9b44783d4556ececdcc9fa
Brian Kendall <brian@briankendall.net>
2019-01-27 10:48:52 -0500

    Initial commit. Dialogue stuff happening!

---
+++ Assets/MainController.cs
@@ -0,0 +1,134 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MainController : MonoBehaviour
+{
+    public GameObject arrow;
+    public GameObject eyesSprite;
+    public GameObject mouthSprite;
+    public GameObject headSprite;
+    public GameObject dialogueBox;
+    public GameObject optionsBox;
+    public Sprite[] mouthSprites;
+    public Sprite eyesShiftySprite;
+    public Sprite eyesGooglySprite;
+    public Sprite eyesSurprisedSprite;
+    
+    [System.Serializable]
+    class State {
+        public float inputX = 0;
+        public float inputY = 0;
+        public float inputFire = 0;
+        
+        public string dialogue = "";
+        public double lastLetterTimestamp = 0;
+        
+        public State Clone() {
+            return (State)MemberwiseClone();
+        }
+    };
+    
+    const double textCharacterEntryDuration = 0.04;
+    const double textCharacterEntryDurationFast = 0.0;
+    const int dialogueMaxLineLength = 28;
+    
+    Text dialogueText;
+    Text optionsText;
+    State state = new State();
+    State previousState = new State();
+    
+    
+    void Start() {
+        dialogueText = dialogueBox.GetComponent<Text>();
+        optionsText = optionsBox.GetComponent<Text>();
+        
+        dialogueText.text = "";
+        optionsText.text = "";
+        arrow.SetActive(false);
+        
+        state.dialogue = "This is some test text. Whoop dee doo! It should appear promptly. Here is some more text. Wheee! This is a thingy.";
+    }
+
+    // Update is called once per frame
+    void Update() {
+        State holdPreviousState = state.Clone();
+        double time = Time.time;
+        state.inputX = Input.GetAxis("Horizontal");
+        state.inputY = Input.GetAxis("Vertical");
+        state.inputFire = Input.GetAxis("Fire1");
+        
+        if (dialogueText.text.Length < state.dialogue.Length) {
+            double nextLetterTimestamp = state.lastLetterTimestamp + ((state.inputFire > 0) ? textCharacterEntryDurationFast : textCharacterEntryDuration);
+            
+            if (time >= nextLetterTimestamp) {
+                char nextLetter = state.dialogue[dialogueText.text.Length];
+                
+                if (nextLetter == ' ') {
+                    int currentLineCount = countLinesInDialogueText(dialogueText.text);
+                    int nextLineCount = countLinesInDialogueText(dialogueText.text + ' ' + findNextWordInTextAfterIndex(state.dialogue, dialogueText.text.Length));
+                    
+                    if (nextLineCount > currentLineCount) {
+                        nextLetter = '\n';
+                    }
+                }
+                
+                dialogueText.text += nextLetter;
+                state.lastLetterTimestamp = time;
+            }
+        }
+        
+        previousState = holdPreviousState;
+    }
+    
+    string findNextWordInTextAfterIndex(string text, int index) {
+        string result = "";
+        
+        for(int i = index+1; i < text.Length; ++i) {
+            if (text[i] == ' ') {
+                break;
+            }
+            
+            result += text[i];
+        }
+        
+        return result;
+    }
+    
+    int countLinesInDialogueText(string text) {
+        int lineCount = 0;
+        int lineCharCount = 0;
+        int positionOfLastSpace = -1;
+        
+        for(int i = 0; i < text.Length; ++i) {
+            lineCharCount += 1;
+            
+            if (text[i] == ' ') {
+                positionOfLastSpace = i;
+            }
+            
+            if (text[i] == '\n') {
+                positionOfLastSpace = -1;
+                lineCharCount = 0;
+                lineCount += 1;
+                continue;
+            }
+            
+            if (lineCharCount <= dialogueMaxLineLength) {
+                continue;
+            }
+            
+            if (positionOfLastSpace != -1) {
+                lineCharCount = i - positionOfLastSpace + 1;
+            } else {
+                lineCharCount = 1;
+            }
+            
+            positionOfLastSpace = -1;
+            lineCount += 1;
+        }
+        
+        return lineCount;
+    }
+}
---
+++ Assets/VoiceSynth.cs
@@ -0,0 +1,161 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VoiceSynth : MonoBehaviour
+{
+    public enum Voice {
+        Voice1 = 0,
+        Voice2 = 1,
+        Voice3 = 2,
+        Voice4 = 3,
+        Voice5 = 4,
+        Voice6 = 5,
+        Voice7 = 6,
+        Voice8 = 7,
+        Voice9 = 8,
+        Voice10 = 9,
+        Voice11 = 10
+    };
+    
+    public enum VoiceStyle {
+        Wide,
+        Normal,
+        Narrow,
+        Monotone,
+        Low,
+        High
+    };
+    
+    class Bucket {
+        public Bucket(int inNumStaleValues) {
+            freshValues = new List<int>();
+            staleValues = new List<int>();
+            numStaleValues = inNumStaleValues;
+        }
+        
+        public void FillBucketWithRange(int min, int exclusiveMax) {
+            freshValues.Clear();
+            staleValues.Clear();
+            
+            for(int i = min; i < exclusiveMax; ++i) {
+                freshValues.Add(i);
+            }
+        }
+        
+        public int Take() {
+            int index = Random.Range(0, freshValues.Count);
+            int value = freshValues[index];
+            freshValues.RemoveAt(index);
+            staleValues.Add(value);
+            
+            while(staleValues.Count > numStaleValues) {
+                int freshAgain = staleValues[0];
+                staleValues.RemoveAt(0);
+                freshValues.Add(freshAgain);
+            }
+            
+            return value;
+        }
+        
+        int numStaleValues;
+        List<int> freshValues;
+        List<int> staleValues;
+    };
+    
+    class VoiceStyleSet {
+        public VoiceStyleSet() {
+            styles = new Dictionary<VoiceStyle, List<AudioClip> >();
+        }
+        
+        public Dictionary<VoiceStyle, List<AudioClip> > styles;
+    };
+    
+    class VoiceSet {
+        public VoiceSet() {
+            speeds = new Dictionary<int, VoiceStyleSet>();
+        }
+        
+        public Dictionary<int, VoiceStyleSet> speeds;
+    };
+    
+    static Dictionary<Voice, VoiceSet> voiceSets = null;
+    
+    public bool talking = false;
+    public Voice voice = Voice.Voice1;
+    public VoiceStyle style = VoiceStyle.Normal;
+    public AudioSource audioSource = null;
+    
+    const double speed1PieceDuration = 0.2;
+    const int numSpeeds = 3;
+    const int numPiecesPerStyle = 8;
+    double nextPlayTime = 0;
+    int lastPieceIndex = -1;
+    Bucket pieceBucket;
+    
+    // Start is called before the first frame update
+    void Awake() {
+        pieceBucket = new Bucket(2);
+        pieceBucket.FillBucketWithRange(0, numPiecesPerStyle);
+        
+        if (voiceSets == null) {
+            loadAllVoiceClips();
+        }
+    }
+
+    void loadAllVoiceClips() {
+        voiceSets = new Dictionary<Voice, VoiceSet>();
+        
+        foreach(Voice voice in System.Enum.GetValues(typeof(Voice))) {
+            if (!voiceSets.ContainsKey(voice)) {
+                voiceSets[voice] = new VoiceSet();
+            }
+            
+            for(int speed = 1; speed <= numSpeeds; ++speed) {
+                if (!voiceSets[voice].speeds.ContainsKey(speed)) {
+                    voiceSets[voice].speeds[speed] = new VoiceStyleSet();
+                }
+            
+                foreach(VoiceStyle style in System.Enum.GetValues(typeof(VoiceStyle))) {
+                    if (!voiceSets[voice].speeds[speed].styles.ContainsKey(style)) {
+                        voiceSets[voice].speeds[speed].styles[style] = new List<AudioClip>();
+                    }
+                
+                    for(int pieceIndex = 0; pieceIndex < numPiecesPerStyle; ++pieceIndex) {
+                        string filename = "voc" + (((int)voice)+1) + "_speed" + speed + "_" + style.ToString().ToLower() + "_" + pieceIndex.ToString("D2");
+                        string path = "voices/voc" + (((int)voice)+1) + "/" + filename;
+                        AudioClip clip = Resources.Load<AudioClip>(path);
+                        
+                        if (clip == null) {
+                            throw new System.Exception("Failed to load voice clip: " + path);
+                        }
+                        
+                        voiceSets[voice].speeds[speed].styles[style].Add(clip);
+                    }
+                }
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void FixedUpdate() {
+        if (audioSource == null || !talking) {
+            return;
+        }
+        
+        playVoicePieceAndQueueNext();
+    }
+    
+    void playVoicePieceAndQueueNext() {
+        double time = Time.realtimeSinceStartup;
+        
+        if (time >= nextPlayTime) {
+            int speed = Random.Range(1, numSpeeds+1);
+            int pieceIndex = pieceBucket.Take();
+            audioSource.PlayOneShot(voiceSets[voice].speeds[speed].styles[style][pieceIndex]);
+            
+            lastPieceIndex = pieceIndex;
+            nextPlayTime = time + speed1PieceDuration / (speed/2.0 + 0.5);
+        }
+    }
+}
